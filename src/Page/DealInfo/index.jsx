8460fd871235f901cd3ed6189c4f0ca574bb935e
Your Name <you@example.com>
2023-02-17 18:00:18 +0800

    update

--- src/Page/DealInfo/index.jsx
+++ src/Page/DealInfo/index.jsx
@@ -11,11 +11,15 @@ export default class DealInfo extends React.Component{
         }
         
     }
-
+ 
+    componentWillUpdate(newProps,newState){
+        newState.disable = newProps.disable;
+    }
+        
     render(){
         const chengeInput= this.props.chengeInput;
         return(
-            this.props.disable?<div className="bg" >
+            this.state.disable?<div className="bg" >
                 <div className="windowsStyle">
                     <div className = "titleStyle"><span style={{fontSize:"18px"}}>新增车辆信息</span><Button type="text" style={{fontSize:"20px",lineHeight:"20px"}} onClick={chengeInput}>×</Button></div>
                     <InputStyle></InputStyle>
